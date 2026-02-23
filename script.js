4ef6b559b7b0bdcf82443e7e4ef911e9cf99ef91
Rishikasingh2 <rishikasingh88k@gmail.com>
2026-02-23 11:51:46 -0800

    1st commite

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+document.write("Hello World");
+
+// PRIMITIVE TYPE
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="Rishika";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
