a470547c301c58991c9ec0c136ccefab31c2ef72
Rishikasingh2 <rishikasingh88k@gmail.com>
2026-02-25 02:44:59 -0800

    5th commite

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ console.log(typeof(num));
 num=undefined;
 console.log(num);
 console.log(typeof(num));
+<script type ="text/script">
+var num =10;
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+function var sayhello=function() {
+    alert("hello world")
+    
+}
+
+const sym2= symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+    </script>
+}
